a321b8b4ad93fa5864c1009eaaf7efae68c5cf9a
Carl Hägglund <calle.hagglund@gmail.com>
2026-04-24 00:49:58 +0200

    final version

---
+++ lib/pages/main_view.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:lab2/widgets/difficulty_control.dart';
+import 'package:lab2/widgets/ingredient_control.dart';
+import 'package:lab2/widgets/kitchen_control.dart';
+import 'package:lab2/widgets/price_control.dart';
+import 'package:lab2/widgets/recipe_list.dart';
+import 'package:lab2/widgets/time_control.dart';
+import 'package:lab2/widgets/recipe_detail.dart';
+import 'package:lab2/ui_controller.dart';
+import 'package:provider/provider.dart';
+
+
+class MainView extends StatelessWidget {
+  const MainView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Row(children: [_controlPanel(context), RecipeArea()]),
+    );
+}
+  Widget _controlPanel(context, {double width = 320}) {
+   return Container(
+    width: width,
+    color: const Color.fromARGB(255, 56, 56, 109),
+    child: Column(
+      children: [
+        Text("Receptsök"),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text("Hitta ett recept som passar genom att ändra inställningarna nedanför"),
+        ),
+        Row(
+          children: [
+            Text("Ingridiens"),
+            IngredientControl(),
+          ],
+        ),
+        Row(
+          children: [
+            Text("Kök"),
+            KitchenControl(),
+          ],
+        ),
+        Text("Svårighetsgrad"),
+        DifficultyControl(),
+
+        Text("Maxpris:"),
+        PriceControl(),
+        Text("Maxtid:"),
+        TimeControl(),
+      ],
+    )
+   );
+}
+
+}
+
+class RecipeArea extends StatelessWidget {
+  const RecipeArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var uiController = context.watch<UIController>();
+    Widget contents;
+
+    if (uiController.showRecipeList) {
+      contents = RecipeList();
+    } else {
+      contents = RecipeDetail(uiController.selectedRecipe!);
+    }
+    return Expanded(child: contents);
+  }
+}
---
+++ lib/widgets/recipe_detail.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:lab2/ui_controller.dart';
+import'package:lab2/model/recipe_database/recipe.dart';
+
+class RecipeDetail extends StatelessWidget {
+  const RecipeDetail(this.recipe, {super.key});
+
+  final Recipe recipe;
+
+  @override
+  Widget build(BuildContext context) {
+    var uiController = Provider.of<UIController>(context, listen: false);
+
+    return Column(
+      children: [
+        IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          uiController.deselectRecipe();
+      },
+    ),
+    SizedBox(
+      width: 400,
+      height: 400,
+      child: recipe.image,
+    ),
+    Text(recipe.name)]);
+}
+}
